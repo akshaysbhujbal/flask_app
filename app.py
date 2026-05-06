daf75c502faa2205239e9d28a001fee927dad881
Akshay Bhujbal <akshaysbhujbal90@gmail.com>
2026-05-06 01:24:08 -0700

    Added '/add' route
    get two values from and adding them

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ app=Flask(__name__)
 def home():
     return 'Welcome to the home page'
 
-@app.route('/api/<name>')
+#Value get from url
+'''@app.route('/api/<name>')
 def name(name):
     length=len(name)
     if length>5:
         return 'Name is too long'
     else:
-        return 'Nice name...'+name
-    
+        return 'Nice name...'+name'''
+
+
+#addition of two number ie get from url
+@app.route('/add/<a>/<b>')
+def add(a,b):
+    answer=int(a)+int(b)
+    result={'ans':answer}
+    return result
 if __name__ =='__main__':
     app.run(debug=True)
