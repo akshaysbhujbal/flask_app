e49b300b07574bd24e6d16023fc82bb4fe81db8d
Akshay Bhujbal <akshaysbhujbal90@gmail.com>
2026-05-06 01:18:33 -0700

    added one more route
    Value get from url

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def home():
     return 'Welcome to the home page'
 
+@app.route('/api/<name>')
+def name(name):
+    length=len(name)
+    if length>5:
+        return 'Name is too long'
+    else:
+        return 'Nice name...'+name
+    
 if __name__ =='__main__':
     app.run(debug=True)
